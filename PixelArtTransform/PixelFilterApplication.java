6ec0078ef27cf23ed8fa653691e45854c91661f9
JingShing <JingShingLai@gmail.com>
2023-04-11 16:53:05 +0800

    Update PixelFilterApplication.java

--- PixelArtTransform/PixelFilterApplication.java
+++ PixelArtTransform/PixelFilterApplication.java
@@ -1,6 +1,7 @@
 package idv.jingshing.pixel.fileter;
 
 public class PixelFilterApplication {
+	// this class is application class you should use this class for init the gui
 	public static void main(String[] args) {
 		GuiHelper gui = new GuiHelper();		
 	}
